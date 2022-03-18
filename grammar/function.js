fa75b85878ca35c0d9949be15ebf668cdad75d45
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2022-03-18 13:58:59 +0900

    function test

---
+++ grammar/function.js
@@ -0,0 +1,14 @@
+function  문자출력함수(문자) {
+    console.log(문자)
+};
+
+문자출력함수('PC지키미');
+
+// printChar = 문자출력함수 //printchar에다 함수 할당
+// printChar('mirim')
+
+printChar = function (문자){
+    console.log(문자)
+};
+
+printChar('청소 꼼꼼히');
