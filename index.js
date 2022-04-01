b89968843a194fc1511f2c6f7f96ffc5ddd7b139
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2022-04-01 14:09:50 +0900

    web2-23 파일목록

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ const app = http.createServer(function (request, response) {
             const title = 'Welcome'
             const description = 'Hello, Node.js'
             const template = `
-<!doctype html>
-<html lang="ko">
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-    </body>
-</html>
+                <!doctype html>
+                <html lang="ko">
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                            <li><a href="/?id=HTML">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                    </body>
+                </html>
 `
             response.writeHead(200)
             response.end(template)
@@ -35,23 +35,23 @@ const app = http.createServer(function (request, response) {
             fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                 const title = queryData.id
                 const template = `
-<!doctype html>
-<html lang="ko">
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-    </body>
-</html>
+                    <!doctype html>
+                    <html lang="ko">
+                        <head>
+                            <title>WEB1 - ${title}</title>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            <ul>
+                                <li><a href="/?id=HTML">HTML</a></li>
+                                <li><a href="/?id=CSS">CSS</a></li>
+                                <li><a href="/?id=JavaScript">JavaScript</a></li>
+                            </ul>
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                        </body>
+                    </html>
 `
                 response.writeHead(200)
                 response.end(template)
